5ab40a4c6036a9cde13975df7faf0378624bcc27
Bahjet <bah.jet@hotmail.com>
2024-01-05 18:29:23 -0300

    button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,7 +3,7 @@ import { ButtonVariant, ButtonContainer } from './Button.styles';
 interface ButtonProps {
     variant?: ButtonVariant;
 };
-
+ 
 export function Button({variant = 'primary'}: ButtonProps){
     return(
         <ButtonContainer variant={variant}>Enviar</ButtonContainer>
