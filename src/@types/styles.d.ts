9b928e8f2593191f23ff0223b5be681c748ee78c
Bahjet <bah.jet@hotmail.com>
2024-01-05 22:58:18 -0300

    parei da parte cores e fonte

---
+++ src/@types/styles.d.ts
@@ -0,0 +1,13 @@
+/*
+    arquivo de definição de tipos
+    codigo totalmente typescript
+*/ 
+import 'styled-components';
+import { defaultTheme } from '../styles/themes/default'
+
+type ThemeType = typeof defaultTheme;
+
+declare module 'styled-components'{
+    export interface DefaultTheme extends ThemeType{   
+    }
+}
